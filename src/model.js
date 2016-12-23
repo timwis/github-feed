1eb6ee872c54c5caaaefd626c85276117ad30ab3
timwis <tim@timwis.com>
2016-12-23 07:44:38 -0500

    fix typo in gatekeeper url

--- src/model.js
+++ src/model.js
@@ -8,7 +8,7 @@ const GitHub = require('github-api')
 const config = {
   GITHUB_AUTH_URL: 'https://github.com/login/oauth/authorize',
   GITHUB_CLIENT: '1d8f959ece6eb757b542',
-  GATEKEEPER_HOST: 'https://github-feed-gatekeeper.herokuapp.com/'
+  GATEKEEPER_HOST: 'https://github-feed-gatekeeper.herokuapp.com'
 }
 
 module.exports = {
